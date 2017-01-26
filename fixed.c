3fe735405f0b37b567ca36825d162ba05e696ece
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-25 22:10:32 -0600

    Made basic implementation of DecOut
    It contains an unrolled loop: potentially faster,
    but it's a bitch to maintain

--- fixed.c
+++ fixed.c
@@ -5,10 +5,40 @@
 
 #include <stdint.h>
 #include "fixed.h"
+#include "ST7735.h"
 
 void ST7735_sDecOut3(int32_t n)
 {
-  // TODO
+    // boundary value checks
+    if(n < -9999 || 9999 < n)
+    {
+        ST7735_OutString(" *.****");
+        return;
+    }
+
+    if(n < 0)
+    {
+        // if negative, abs() it and print a dash
+        n = -n;
+        ST7735_OutChar('-');
+    }
+
+    // to unroll a loop or not unroll a loop, that is the question
+    int32_t currentDigit = n/1000;
+    ST7735_OutChar('0' + currentDigit);
+    ST7735_OutChar('.');
+    n = n - currentDigit * 1000;
+
+    currentDigit = n/100;
+    ST7735_OutChar('0' + currentDigit);
+    n = n - currentDigit * 100;
+
+    currentDigit = n/10;
+    ST7735_OutChar('0' + currentDigit);
+    n = n - currentDigit * 10;
+
+    // n is already < 10
+    ST7735_OutChar('0' + n);
 }
 
 void ST7735_uBinOut8(uint32_t n)

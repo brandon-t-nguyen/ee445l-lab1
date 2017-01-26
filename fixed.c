3386610c2a92e38088d92fdac9e9659339ac3c5c
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-25 22:48:58 -0600

    Added uBinOut8
    It's messy as shit, but it works

--- fixed.c
+++ fixed.c
@@ -59,7 +59,42 @@ void ST7735_sDecOut3(int32_t n)
 
 void ST7735_uBinOut8(uint32_t n)
 {
-  // TODO
+    if(n >= 256000)
+    {
+        ST7735_OutString("***.**");
+        return;
+    }
+    // effectively what was specified was a 24_8 fixed point format
+    
+    // print the integer part first
+    uint32_t integer = n >> 8;
+    for(int32_t divider=100; integer >= 10; divider/=10)
+    {
+        int32_t currentDigit = integer/divider;
+        // only print a digit if it's not a 0
+        if( currentDigit != 0 )
+            ST7735_OutChar('0' + currentDigit);
+        else
+            ST7735_OutChar(' ');
+        integer -= currentDigit * divider;
+    }
+    ST7735_OutChar('0' + integer);  // capture the 1's place
+    ST7735_OutChar('.');
+
+    
+    // extract the fraction bits
+    uint32_t fraction = n & 0xFF;
+
+    // convert the fraction to decimal form by multiplying by 100 then dividing by 256
+    uint32_t decimal = (fraction * 100) >> 8;
+
+    // print out the fraction part
+    for(int32_t divider=10; divider > 0; divider/=10)
+    {
+        int32_t currentDigit = decimal/divider;
+        ST7735_OutChar('0' + currentDigit);
+        decimal -= currentDigit * divider;
+    }
 }
 
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY)

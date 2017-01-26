251596f61a72a9a17c00c8d1f887ee7b859d8b43
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-25 21:45:45 -0600

    Added necessary files; replaced main file
    Removed ST7735TestMain.c since its unneeded

---
+++ fixed.c
@@ -0,0 +1,31 @@
+// ******** fixed.c ************** 
+// possible header file for Lab 1 Spring 2017
+// feel free to change the specific syntax of your system
+// put your name here
+// put the date here
+
+#include <stdint.h>
+#include "fixed.h"
+
+void ST7735_sDecOut3(int32_t n)
+{
+  // TODO
+}
+
+void ST7735_uBinOut8(uint32_t n)
+{
+  // TODO
+}
+
+void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY)
+{
+  // TODO
+}
+
+void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[])
+{
+  // TODO
+}
+
+
+

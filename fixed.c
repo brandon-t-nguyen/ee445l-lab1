0912439399145fa677be3622d4f083f9d26f4fd3
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-26 16:33:47 -0600

    4 -> 2 space tabs

--- fixed.c
+++ fixed.c
@@ -9,96 +9,96 @@
 
 void ST7735_sDecOut3(int32_t n)
 {
-    // boundary value checks
-    if(n < -9999 || 9999 < n)
-    {
-        ST7735_OutString(" *.****");
-        return;
-    }
+  // boundary value checks
+  if(n < -9999 || 9999 < n)
+  {
+    ST7735_OutString(" *.****");
+    return;
+  }
 
-    if(n < 0)
-    {
-        // if negative, abs() it and print a dash
-        n = -n;
-        ST7735_OutChar('-');
-    }
+  if(n < 0)
+  {
+    // if negative, abs() it and print a dash
+    n = -n;
+    ST7735_OutChar('-');
+  }
 
-    // to unroll a loop or not unroll a loop, that is the question
-    int32_t currentDigit = n/1000;
+  // to unroll a loop or not unroll a loop, that is the question
+  int32_t currentDigit = n/1000;
+  ST7735_OutChar('0' + currentDigit);
+  ST7735_OutChar('.');
+  n = n - currentDigit * 1000;
+  
+  // here's a unrolled and rolled implementation
+  //#define UNROLL
+  #if defined UNROLL
+  //////////////////////////////////////////////////
+  currentDigit = n/100;
+  ST7735_OutChar('0' + currentDigit);
+  n = n - currentDigit * 100;
+
+  currentDigit = n/10;
+  ST7735_OutChar('0' + currentDigit);
+  n = n - currentDigit * 10;
+
+  // n is already < 10
+  ST7735_OutChar('0' + n);
+  //////////////////////////////////////////////////
+  #undef UNROLL
+  #else
+  //////////////////////////////////////////////////
+  for(int32_t divider=100; divider > 0; divider/=10)
+  {
+    currentDigit = n/divider;
     ST7735_OutChar('0' + currentDigit);
-    ST7735_OutChar('.');
-    n = n - currentDigit * 1000;
-    
-    // here's a unrolled and rolled implementation
-    //#define UNROLL
-    #if defined UNROLL
-    //////////////////////////////////////////////////
-    currentDigit = n/100;
-    ST7735_OutChar('0' + currentDigit);
-    n = n - currentDigit * 100;
-
-    currentDigit = n/10;
-    ST7735_OutChar('0' + currentDigit);
-    n = n - currentDigit * 10;
-
-    // n is already < 10
-    ST7735_OutChar('0' + n);
-    //////////////////////////////////////////////////
-    #undef UNROLL
-    #else
-    //////////////////////////////////////////////////
-    for(int32_t divider=100; divider > 0; divider/=10)
-    {
-        currentDigit = n/divider;
-        ST7735_OutChar('0' + currentDigit);
-        n = n - currentDigit * divider;
-    }
-    //////////////////////////////////////////////////
-    #endif
+    n = n - currentDigit * divider;
+  }
+  //////////////////////////////////////////////////
+  #endif
 }
 
 void ST7735_uBinOut8(uint32_t n)
 {
-    if(n >= 256000)
+  if(n >= 256000)
+  {
+    ST7735_OutString("***.**");
+    return;
+  }
+  // effectively what was specified was a 24_8 fixed point format
+  
+  // print the integer part first
+  uint32_t integer = n >> 8;
+  for(int32_t divider=100; integer >= 10; divider/=10)
+  {
+    int32_t currentDigit = integer/divider;
+    // only print a digit if it's not a 0
+    if( currentDigit != 0 )
     {
-        ST7735_OutString("***.**");
-        return;
+      ST7735_OutChar('0' + currentDigit);
     }
-    // effectively what was specified was a 24_8 fixed point format
-    
-    // print the integer part first
-    uint32_t integer = n >> 8;
-    for(int32_t divider=100; integer >= 10; divider/=10)
+    else
     {
-        int32_t currentDigit = integer/divider;
-        // only print a digit if it's not a 0
-        if( currentDigit != 0 )
-        {
-            ST7735_OutChar('0' + currentDigit);
-        }
-        else
-        {
-            ST7735_OutChar(' ');
-        }
-        integer -= currentDigit * divider;
+      ST7735_OutChar(' ');
     }
-    ST7735_OutChar('0' + integer);  // capture the 1's place
-    ST7735_OutChar('.');
+    integer -= currentDigit * divider;
+  }
+  ST7735_OutChar('0' + integer);  // capture the 1's place
+  ST7735_OutChar('.');
 
-    
-    // extract the fraction bits
-    uint32_t fraction = n & 0xFF;
+  
+  // extract the fraction bits
+  uint32_t fraction = n & 0xFF;
 
-    // convert the fraction to decimal form by multiplying by 100 then dividing by 256
-    uint32_t decimal = (fraction * 100) >> 8;
+  // convert the fraction to decimal form by multiplying by 100 then dividing by 256
+  uint32_t decimal = (fraction * 100) >> 8;
 
-    // print out the fraction part
-    for(int32_t divider=10; divider > 0; divider/=10)
-    {
-        int32_t currentDigit = decimal/divider;
-        ST7735_OutChar('0' + currentDigit);
-        decimal -= currentDigit * divider;
-    }
+  // print out the fraction part
+  for(int32_t divider=10; divider > 0; divider/=10)
+  {
+    int32_t currentDigit = decimal/divider;
+    ST7735_OutChar('0' + currentDigit);
+    decimal -= currentDigit * divider;
+  }
 }
 
 // Note that we want to fit the title
@@ -122,79 +122,79 @@ static int32_t MinX, MaxX, MinY, MaxY, VirtualW, VirtualH;
 // This is currently set up for auto-scaling of axes
 static void convertCoord( const int32_t vX, const int32_t vY, uint32_t *paX, uint32_t *paY )
 {
-    // get our virtual offsets
-    int32_t vOffsetX = vX - MinX;
-    int32_t vOffsetY = vY - MinY;
+  // get our virtual offsets
+  int32_t vOffsetX = vX - MinX;
+  int32_t vOffsetY = vY - MinY;
 
-    // get our absolute pixel offsets
-    int32_t aOffsetX = vOffsetX * LCD_WIDTH  / VirtualW;
-    int32_t aOffsetY = vOffsetY * LCD_HEIGHT / VirtualH;
+  // get our absolute pixel offsets
+  int32_t aOffsetX = vOffsetX * LCD_WIDTH  / VirtualW;
+  int32_t aOffsetY = vOffsetY * LCD_HEIGHT / VirtualH;
 
-    // get our final pixel values
-    *paX = aOffsetX;
-    *paY = LCD_HEIGHT - aOffsetY;
-    
-    if( vX < MinX || vX > MaxX )
-    {
-        *paX = OUT_OF_BOUNDS;
-    }
-    if( vY < MinY || vY > MaxY )
-    {
-        *paY = OUT_OF_BOUNDS;
-    }
+  // get our final pixel values
+  *paX = aOffsetX;
+  *paY = LCD_HEIGHT - aOffsetY;
+  
+  if( vX < MinX || vX > MaxX )
+  {
+    *paX = OUT_OF_BOUNDS;
+  }
+  if( vY < MinY || vY > MaxY )
+  {
+    *paY = OUT_OF_BOUNDS;
+  }
 }
 
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY)
 {
-    MinX = minX;
-    MaxX = maxX;
-    MinY = minY;
-    MaxY = maxY;
-    int numLines = 1;                   // first line takes up text
-    char *c = title;
-    while(*c)
+  MinX = minX;
+  MaxX = maxX;
+  MinY = minY;
+  MaxY = maxY;
+  int numLines = 1;           // first line takes up text
+  char *c = title;
+  while(*c)
+  {
+    if(*c=='\n')
     {
-        if(*c=='\n')
-        {
-            ++numLines;  // count number of lines
-        }
-        ++c;
+      ++numLines;  // count number of lines
     }
+    ++c;
+  }
 
-    OriginY = CHAR_HEIGHT * numLines;
-    PlotH = LCD_HEIGHT - OriginY;
-    VirtualW = MaxX-MinX;
-    VirtualH = MaxY-MinY;
+  OriginY = CHAR_HEIGHT * numLines;
+  PlotH = LCD_HEIGHT - OriginY;
+  VirtualW = MaxX-MinX;
+  VirtualH = MaxY-MinY;
 
-    ST7735_SetCursor(0,0);
-    ST7735_OutString(title);
-    ST7735_FillRect(OriginX,OriginY,PlotW,PlotH,0xFFFF); // white out the plot area
+  ST7735_SetCursor(0,0);
+  ST7735_OutString(title);
+  ST7735_FillRect(OriginX,OriginY,PlotW,PlotH,0xFFFF); // white out the plot area
 
-    // draw our axis lines
-    uint32_t xAxis;
-    uint32_t yAxis;
-    convertCoord(0,0,&xAxis,&yAxis);
-    if( xAxis != OUT_OF_BOUNDS)
-    {
-        ST7735_DrawFastVLine(xAxis,OriginY,PlotH,AXIS_COLOR);
-    }
-    if( yAxis != OUT_OF_BOUNDS)
-    {
-        ST7735_DrawFastHLine(OriginX,yAxis,PlotW,AXIS_COLOR);
-    }
+  // draw our axis lines
+  uint32_t xAxis;
+  uint32_t yAxis;
+  convertCoord(0,0,&xAxis,&yAxis);
+  if( xAxis != OUT_OF_BOUNDS)
+  {
+    ST7735_DrawFastVLine(xAxis,OriginY,PlotH,AXIS_COLOR);
+  }
+  if( yAxis != OUT_OF_BOUNDS)
+  {
+    ST7735_DrawFastHLine(OriginX,yAxis,PlotW,AXIS_COLOR);
+  }
 }
 
 void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[])
 {
-    uint32_t x, y;
-    for(uint32_t i = 0; i < num; ++i)
+  uint32_t x, y;
+  for(uint32_t i = 0; i < num; ++i)
+  {
+    convertCoord(bufX[i],bufY[i],&x,&y);
+    if( x != OUT_OF_BOUNDS && y != OUT_OF_BOUNDS )
     {
-        convertCoord(bufX[i],bufY[i],&x,&y);
-        if( x != OUT_OF_BOUNDS && y != OUT_OF_BOUNDS )
-        {
-            ST7735_DrawPixel(x,y,PLOT_COLOR);
-        }
+      ST7735_DrawPixel(x,y,PLOT_COLOR);
     }
+  }
 }
 
 

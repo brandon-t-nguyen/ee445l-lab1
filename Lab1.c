8ffea1478c0c146966431d37b50d9da8155c471e
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-01-31 16:30:15 -0600

    Added our mod dates

--- Lab1.c
+++ Lab1.c
@@ -6,6 +6,10 @@
 // Possible main program to test the lab
 // Feel free to edit this to match your specifications
 
+
+// Modified by Brandon Nguyen and Ryan Syed
+// 1/31/2017
+
 // Backlight (pin 10) connected to +3.3 V
 // MISO (pin 9) unconnected 
 // SCK (pin 8) connected to PA2 (SSI0Clk)
@@ -94,6 +98,8 @@ const int32_t StarXbuf[50] = {0, -6, -12, -18, -24, -30, -35, -41, -47, -53, 59,
 const int32_t StarYbuf[50] = {190, 172, 154, 136, 118, 100, 81, 63, 45, 27, 9, 27, 45, 63, 81, 100, 118, 136, 154, 172, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 9, 20, 31, 43, 54, 65, 76, 87, 99, 110, 121, 110, 99, 87, 76, 65, 54, 43, 31, 20
 };
 
+#define ARRAYLEN(x) (sizeof(x)/sizeof(x[0]))
+
 int main(void){uint32_t i;
   PLL_Init(Bus80MHz);
   PortF_Init();
@@ -102,7 +108,7 @@ int main(void){uint32_t i;
     ST7735_FillScreen(ST7735_BLACK); 
     ST7735_SetCursor(0,0);
     printf("Lab 1\rST7735_sDecOut3\r");
-    for(i=0; i<13; i++){
+    for(i=0; i<ARRAYLEN(outTests1); i++){
       ST7735_sDecOut3(outTests1[i].InNumber);  // your solution
       ST7735_OutString((char*)outTests1[i].OutBuffer); // expected solution
     }
@@ -111,7 +117,7 @@ int main(void){uint32_t i;
     ST7735_FillScreen(0);  // set screen to black
     ST7735_SetCursor(0,0);
     printf("ST7735_uBinOut8\r");
-    for(i=0; i<14; i++){
+    for(i=0; i<ARRAYLEN(outTests1); i++){
       ST7735_uBinOut8(outTests2[i].InNumber);  // your solution
       ST7735_OutString((char*)outTests2[i].OutBuffer); // expected solution
     }
@@ -123,7 +129,7 @@ int main(void){uint32_t i;
     
     ST7735_XYplotInit("Star- upper right",-450, 150, -400, 200);
     ST7735_XYplot(50,(int32_t *)StarXbuf,(int32_t *)StarYbuf);
-    Pause(); 
+    Pause();
   } 
 } 
 
@@ -139,22 +145,6 @@ void PortF_Init(void){
   GPIO_PORTF_DIR_R |= 0x04;         // 5) set direction to output
   GPIO_PORTF_AFSEL_R &= ~0x14;      // 6) regular port function
   GPIO_PORTF_DEN_R |= 0x14;         // 7) enable digital port
-}
-
-
-int main4(void){ 
-  PortF_Init();
-  while(1){
-    DelayWait10ms(1);
-    PF2 ^= 0x04;
-  }
-}
-int main5(void){ 
-  PortF_Init();
-  while(1){
-    DelayWait10ms(100);
-    PF2 ^= 0x04;
-  }
 }
 
 // Subroutine to wait 10 msec
